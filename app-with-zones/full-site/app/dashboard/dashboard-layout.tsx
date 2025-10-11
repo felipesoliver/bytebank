fb430fa2a55a5e32d5b2ff4d5930fa875e27252e
Felipe Oliveira <foliveira@futurebrand.com>
2025-10-11 12:54:18 -0300

    feat: Improves initial motion

--- app-with-zones/full-site/app/dashboard/dashboard-layout.tsx
+++ app-with-zones/full-site/app/dashboard/dashboard-layout.tsx
@@ -1,30 +1,23 @@
 'use client'
 
 import React from 'react'
-
-import { useEffect, useState } from 'react';
 import AsideNav from '@/layouts/blocks/aside-nav';
 import DashboardHero from '@/layouts/blocks/dashboard-hero';
 import TransactionCard from '@/layouts/blocks/transactions';
 import BankStatement from '@/layouts/blocks/bank-statement';
+import { twMerge } from 'tailwind-merge';
+import { RootState } from '@/store';
+import { useSelector } from 'react-redux';
 
 const DashboardLayout = () => {
-  const [firstVisit, setFirstVisit] = useState(false);
-
-  useEffect(() => {
-    const visited = sessionStorage.getItem('dashboardVisited');
-    if (!visited) {
-      setFirstVisit(true); // ativa animação na primeira visita
-      sessionStorage.setItem('dashboardVisited', 'true');
-    }
-  }, []);
+  const authStatus = useSelector((state: RootState) => state.auth.isLoggedIn);
 
   return (
     <div
-      className={`transition-all duration-1000 ease-in-out transform ${
-        firstVisit ? 'opacity-0 -translate-y-5' : 'opacity-100 translate-y-0'
-      }`}
-      onAnimationEnd={() => setFirstVisit(false)} // remove a animação depois
+      className={twMerge(
+        'opacity-0',
+        authStatus && 'animate-fadeup',
+      )}
     >
       <div className="container grid grid-cols-1 lg:grid-cols-12 gap-5 py-5">
         <AsideNav />
